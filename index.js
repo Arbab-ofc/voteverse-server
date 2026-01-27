eccdde6a0f00df8ec8abaa706e3759af82053340
arbabprvt-ofc <arbabprvt@gmail.com>
2026-01-27 23:18:58 +0530

    Add health check endpoint

--- index.js
+++ index.js
@@ -41,6 +41,9 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 
 app.get('/', (req, res) => {
   res.send('VoteVerse API is running...');
